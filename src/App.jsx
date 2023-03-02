1153da4646102713a48a06c2bc834a0710e4b891
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-03-02 18:55:33 +0200

    feature: add header with adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Cart from './pages/Cart'
 import Home from './pages/Home'
@@ -5,9 +6,13 @@ import Home from './pages/Home'
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Cart />
+      <div className="wrapper">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+      </div>
     </div>
   )
 }
